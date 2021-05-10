5e894bc3875ac0bb8633da9e17636be6b86d795d
NINJA-Ai <68237917+NINJA-Ai@users.noreply.github.com>
2021-05-10 17:19:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -18,12 +18,17 @@ document.getElementById("result").innerHTML = '<img id="captured_image" src="'+d
 }
 console.log('ml5 version: ',ml5.version);
 
-classifier = ml5.imageClassifier('/model.json', modelLoaded );
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json', modelLoaded );
 
 function modelLoaded()
 
 {
     console.log("modelLoaded!");
+}
+function english()
+{
+img = document.getElementById('captured_image');
+classifier.classify(img, gotresult);
 }
 function english()
 {
